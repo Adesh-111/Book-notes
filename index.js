edae123fd0fd6626338f9b864bc54502625ae04d
Adesh-111 <adeshabinesh25@gmail.com>
2024-05-14 21:05:13 +0530

    Add autherization for database

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 import express from "express";
 import pg from "pg";
 import bodyParser from "body-parser";
+import env from "dotenv";
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static("public"));
+env.config();
 
 const db = new pg.Client({
-    user : "postgres",
-    host : "localhost",
-    database : "Books",
-    password : "adeshabin1251",
-});
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+  });
 
 db.connect();
 
